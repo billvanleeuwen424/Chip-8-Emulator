3f4c52b672360c2921015106f14a3d8e6173227e
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2022-07-24 12:11:51 -0400

    creating constructors for chip8

--- code/lib/chip8.hpp
+++ code/lib/chip8.hpp
@@ -4,6 +4,7 @@
 class chip8{
     public:
         //unsigned char is an 8 bit datatype
-        unsigned char memory[MEMORYSIZE];
-        unsigned char generalRegisters[REGISTERCOUNT];
+        unsigned char memory[MEMORYSIZE] = {0};
+        unsigned char generalRegisters[REGISTERCOUNT] = {0};
+
 };
