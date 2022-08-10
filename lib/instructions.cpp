ce45af615f531773103b2d93aa615403ece9865e
Bill Van Leeuwen <billvanleeuwen424@gmail.com>
2022-08-09 21:04:47 -0400

    comments, formatting

--- lib/instructions.cpp
+++ lib/instructions.cpp
@@ -1,3 +1,15 @@
+/**
+ * @brief  
+ * 6xkk - LD Vx, byte
+ * Set Vx = kk.
+ * The interpreter puts the value kk into register Vx.
+ * 
+ * @param  pRegister: a pointer to the deisred register
+ * @param  loadValue:  the value to asign to that register
+ */
+void LD_6xkk(unsigned char * pRegister, unsigned char loadValue){
+    *pRegister = loadValue;
+}
 
 /**
  * @brief 
@@ -10,8 +22,4 @@
  */
 void ADD_7xkk(unsigned char * pRegister, unsigned char additionValue){
     *pRegister += additionValue;
-}
-
-void LD_6xkk(unsigned char * pRegister, unsigned char loadValue){
-    *pRegister = loadValue;
 }
