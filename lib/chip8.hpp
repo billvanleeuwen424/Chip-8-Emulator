6e9e752a39060349e7d0c5d4ef66dd11435b4e43
Bill Van Leeuwen <billvanleeuwen424@gmail.com>
2022-08-26 13:26:40 -0400

    add base method

--- lib/chip8.hpp
+++ lib/chip8.hpp
@@ -3,8 +3,16 @@
 
 struct chip8{
     public:
+
+        //program in RAM starts at 0x200
+        unsigned short pc = 0x200;
+
         //unsigned char is an 8 bit datatype
         unsigned char memory[MEMORYSIZE] = {0};
         unsigned char generalRegisters[REGISTERCOUNT] = {0};
 
+
+        unsigned char fetch(unsigned short pc){
+            return 0;
+        }
 };
