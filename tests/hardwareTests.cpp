bd017c2c4795bd2532ca681abe2bea9f17996d91
Bill Van Leeuwen <billvanleeuwen424@gmail.com>
2022-08-31 05:27:38 -0400

    Decode - 8 functions

--- tests/hardwareTests.cpp
+++ tests/hardwareTests.cpp
@@ -2,6 +2,7 @@
 
 #include "chip8.hpp"
 #include "constants.hpp"
+#include "instructions.hpp"
 
 TEST_CASE("Test the Fetch method"){
     chip8 testChip;
@@ -18,4 +19,17 @@ TEST_CASE("Test the Fetch method"){
         REQUIRE(testChip.fetch(MEMORYSIZE+1) == 0);
     }
     
+}
+
+TEST_CASE("Test the Decode method"){
+    chip8 testChip;
+
+    SECTION("Correctly decode, and perform correct 8-opcode function"){
+        testChip.generalRegisters[1] = 0x5;
+
+        testChip.decodeExecute(0x8010);
+
+        REQUIRE(testChip.generalRegisters[0] == 0x5);
+    }
+
 }
