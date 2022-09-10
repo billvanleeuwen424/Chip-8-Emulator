d0ad70814a40699a533319f56a9c8c4244c0f5b1
Bill Van Leeuwen <billvanleeuwen424@gmail.com>
2022-09-10 15:19:26 -0400

    Refactoring old C types to stdint types

--- lib/instructions.hpp
+++ lib/instructions.hpp
@@ -1,21 +1,21 @@
-void JP_1nnn(unsigned short *programCounter, unsigned short jumpDestination);
+void JP_1nnn(u_int16_t *programCounter, u_int16_t jumpDestination);
 
-void CALL_2nnn(unsigned short *programCounter, unsigned short * stackPointer, unsigned short callDestination);
+void CALL_2nnn(u_int16_t *programCounter, u_int16_t * stackPointer, u_int16_t callDestination);
 
-void SE_3xkk(unsigned short *programCounter, unsigned char * pRegister, unsigned char checkValue);
+void SE_3xkk(u_int16_t *programCounter, u_int8_t * pRegister, u_int8_t checkValue);
 
-void SNE_4xkk(unsigned short *programCounter, unsigned char * pRegister, unsigned char checkValue);
+void SNE_4xkk(u_int16_t *programCounter, u_int8_t * pRegister, u_int8_t checkValue);
 
-void LD_6xkk(unsigned char * pRegister, unsigned char loadValue);
+void LD_6xkk(u_int8_t * pRegister, u_int8_t loadValue);
 
-void ADD_7xkk( unsigned char * pRegister, unsigned char additionValue);
+void ADD_7xkk( u_int8_t * pRegister, u_int8_t additionValue);
 
-void LD_8xy0(unsigned char * pRegisterx, unsigned char * pRegistery);
+void LD_8xy0(u_int8_t * pRegisterx, u_int8_t * pRegistery);
 
-void OR_8xy1(unsigned char * pRegisterx, unsigned char * pRegistery);
+void OR_8xy1(u_int8_t * pRegisterx, u_int8_t * pRegistery);
 
-void AND_8xy2(unsigned char * pRegisterx, unsigned char * pRegistery);
+void AND_8xy2(u_int8_t * pRegisterx, u_int8_t * pRegistery);
 
-void XOR_8xy3(unsigned char * pRegisterx, unsigned char * pRegistery);
+void XOR_8xy3(u_int8_t * pRegisterx, u_int8_t * pRegistery);
 
-void ADD_8xy4(unsigned char * pRegisterx, unsigned char * pRegistery, unsigned char * pRegisterF);
+void ADD_8xy4(u_int8_t * pRegisterx, u_int8_t * pRegistery, u_int8_t * pRegisterF);
